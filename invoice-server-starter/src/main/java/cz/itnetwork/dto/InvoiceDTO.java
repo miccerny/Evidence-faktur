1ac6b020120045d3fb30a16eff56a261db591169
Michal Černý <cerny.michal93@gmail.com>
2025-05-17 15:12:20 +0200

    update

--- invoice-server-starter/src/main/java/cz/itnetwork/dto/InvoiceDTO.java
+++ invoice-server-starter/src/main/java/cz/itnetwork/dto/InvoiceDTO.java
@@ -2,6 +2,7 @@ package cz.itnetwork.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,19 +24,37 @@ public class InvoiceDTO {
     @JsonProperty("_id")
     private Long id;
 
-
+    @NotEmpty(message = "Číslo faktury nesmí být prázdné")
     private String invoiceNumber;
+
+    @NotNull(message = "Dodavatel musí být vyplněn")
     private PersonDTO seller;
+    @NotNull(message = "Odběratel musí být vyplněn")
     private PersonDTO buyer;
 
+    @FutureOrPresent
+    @NotNull(message = "Datum musí být vyplněno")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate issued;
+
+    @Future
+    @NotNull(message = "Datum musí být vyplněno")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate dueDate;
 
+    @NotEmpty(message = "Název productu nesmí být prázdný")
     private String product;
+
+    @Min(value = 1, message = "Cena musí být alespoň 1")
+    @NotNull(message = "Cena musí být vyplněna")
     private float price;
+
+    @NotNull(message = "DPH musí být vyplněno")
+    @Min(value = 0, message = "DPH musí být minimálně 0")
+    @Max(value = 100, message = "DPH může být maximálně 100")
     private double vat;
+
+
     private String note;
 
 }
